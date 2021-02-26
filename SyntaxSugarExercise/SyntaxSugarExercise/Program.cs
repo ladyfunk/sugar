5a62dec7371667cffff1da624db803b9be70f08b
Oana b <oanabij@gmail.com>
2021-02-26 23:42:17 +0000

    initial commit

---
+++ SyntaxSugarExercise/SyntaxSugarExercise/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SyntaxSugarExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = 4;
+            string response;
+
+            {
+                response = ( answer < 9) ?  " is less than nine" : "greater than or equal to nine";
+            }
+            
+            Console.WriteLine($"{answer} {response}");
+
+        }
+    }
+}
